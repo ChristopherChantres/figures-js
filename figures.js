578fad580b53362448f92150ed759a94ac661185
ChristopherChantres <chris.chantres@gmail.com>
2022-03-30 11:44:43 -0400

    Square code is done

--- figures.js
+++ figures.js
@@ -1,1 +1,10 @@
-console.log('Hello world');
+// Square Code
+
+const squareSide = 6;
+console.log('The square side is: ' + squareSide);
+
+const squarePerimeter = squareSide * 4;
+console.log('The square perimeter is: ' + squarePerimeter);
+
+const squareArea = squareSide * squareSide;
+console.log('The square area is: ' + squareArea);
